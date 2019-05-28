1bae2c421cc38b49b6cc0f25980346507080a3d3
Mayadag <mayadag@hotmail.de>
2019-05-28 20:33:13 +0100

    Update PCA of Treated and PCA of topVars of Treated.R

--- PCA of Treated and PCA of topVars of Treated.R
+++ PCA of Treated and PCA of topVars of Treated.R
@@ -1,8 +1,10 @@
 # PCA OF TREATED CELL LINES
 
+
 # These were my considerations regarding the PCA of treated cell lines,
 # as well as the PCA of variables above the 75% quantile 
 
+########################### INSTALLING DATA  #########################    
 install.packages("ggfortify")
 library(readr)
 library(ggplot2)
@@ -10,66 +12,65 @@ library("ggfortify")
 library(rstudioapi)
 
 wd = dirname(rstudioapi::getSourceEditorContext()$path)
-df_treated = as.data.frame( t(readRDS(paste0(wd,"/data/NCI_TPW_gep_treated.rds"))))
+t_df_treated = as.data.frame( t(readRDS(paste0(wd,"/data/NCI_TPW_gep_treated.rds"))))
 
-
+########################### PREPARING CELL LINE NAMES AND DRUG NAMES #########################   
 # In order to add color the PCA results, we will need a column in our data frames with 
-# cell line names OR drug names (see: https://cran.r-project.org/web/packages/ggfortify/vignettes/plot_pca.html)
+# cell line names OR drug names 
+# (see: "https://cran.r-project.org/web/packages/ggfortify/vignettes/plot_pca.html" )
 
-# Cut cell_line_names and drug names [l]. 
-m = as.data.frame(strsplit( x= row.names(df_treated), split= "_"))
+# Divide colnames into 1- Cell_line, 2- Drug_name, 3- Dosage, 4- Time_point
+m = as.data.frame(strsplit( x= row.names(t_df_treated), split= "_"))
+# Make a data frame which consists of a single column with cell_line names
 cell_line_names = as.data.frame (t(m[1,]))
-drug_names = as.data.frame (t(m[2,]))
-
-
-# Create 2 different data with an additional coloum each, either with cell_line_names or drug_names
-# And order the rows according to cell_line_names or genes_names
-
-df_treated_col_cellline = df_treated
+# Create a new df from the tranposed df_treated with an additional coloumn of cell_line_names
+df_treated_col_cellline = t_df_treated
 df_treated_col_cellline$cell_line = cell_line_names[,1]
-df_treated_col_cellline = df_treated_col_cellline[ order(df_treated_col_cellline$cell_line),]
 
-df_treated_col_drug_name = df_treated
+
+# Make a data frame which consists of a single column with drug_names
+drug_names = as.data.frame (t(m[2,]))
+# Create a  data frame from transposed df_treated df with an additional coloumn of drug_names
+df_treated_col_drug_name = t_df_treated
 df_treated_col_drug_name$drug = drug_names[,1]
-df_treated_col_drug_name = df_treated_col_drug_name[ order(df_treated_col_drug_name$drug),]
 
-# Start the PCA
-pca_df_treated = prcomp( df_treated, center =F, scale = F)
-# Centering and Scaling are left out, as this could cause 
-# an information loss concerning batching effect 
+########################### PCA OF TREATED CELL LINES #########################    
+
+pca_df_treated = prcomp( df_treated, center =T, scale = T)
+
 autoplot(pca_df_treated)
 
 plot(pca_df_treated , type = "l") # First 2 PC are most relevant
 
-# How do i get colours for EACH DRUG 
+# How to add colours for EACH DRUG 
 autoplot(pca_df_treated, data= df_treated_col_drug_name, colour = 'drug')
 
-# How do i get colours for EACH CELL LINE
+# How to add colours for EACH CELL LINE
 autoplot(pca_df_treated, data= df_treated_col_cellline , colour = 'cell_line')
 
+########################### PCA OF TREATED  > Q75% ########################
 # PCA OF TREATED CELL LINES WITH A VARIANCE HIGHER THAN THE 75% OF VARIANCES
-topVar = apply(df_treated, 2, var)
+ 
+topVar = apply(t_df_treated, 2, var)
 
 #Select the genes which have a Variance higher than 75% of the variances
-df_treated_topVar = df_treated[ topVar > quantile(topVar, probs =  0.75)]
+df_treated_topVar = t_df_treated[ topVar > quantile(topVar, probs =  0.75)]
 
 
-df_Tre_topVar_col_cellline = df_treated_topVar
-df_Tre_topVar_col_cellline$cell_line = cell_line_names[,1]
-df_Tre_topVar_col_cellline = df_treated_topVar[ order(df_Tre_topVar_col_cellline$cell_line),]
+df_Tre_topVar_cellline = df_treated_topVar
+df_Tre_topVar_cellline$cell_line = cell_line_names[,1]
 
-df_Tre_topVar_col_drug_name = df_treated_topVar
-df_Tre_topVar_col_drug_name$drug = drug_names[,1]
-df_Tre_topVar_col_drug_name = df_Tre_topVar_col_drug_name[ order(df_Tre_topVar_col_drug_name$drug),]
+df_Tre_topVar_drug_name = df_treated_topVar
+df_Tre_topVar_drug_name$drug = drug_names[,1]
 
-# Start the PCA  
-pca_df_Tre_topVar = prcomp( df_treated_topVar, center =F, scale = F)
+# Start the PCA  of topVar
+pca_df_Tre_topVar = prcomp( df_treated_topVar, center =T, scale = T)
 autoplot(pca_df_Tre_topVar)
-plot(pca_df_Tre_topVar, type = "l")    # First 2 PC are most relevant
+plot(pca_df_Tre_topVar, type = "l")    # First 5 PCs are most relevant
 
-# How do i get colours for EACH DRUG 
-autoplot(pca_df_Tre_topVar, data= df_Tre_topVar_col_drug_name, colour = 'drug')
+# How to add colours for EACH DRUG 
+autoplot(pca_df_Tre_topVar, data= df_Tre_topVar_drug_name, colour = 'drug')
 
-# How do i get colours for EACH CELL LINE
-autoplot(pca_df_treated, data= df_treated_col_cellline , colour = 'cell_line')
+# How to add colours for EACH CELL LINE
+autoplot(pca_df_Tre_topVar, data= df_Tre_topVar_cellline , colour = 'cell_line')
 
