06ed741b21688ea8406c4d06073c93d33828e0e5
Laura Plutowski <laura.plutki@gmail.com>
2019-06-01 20:51:58 +0200

    idea for fold change

---
+++ How to save a picture as Pdf.R
@@ -0,0 +1,4 @@
+############### How to save a picture ###############
+#pdf( "/Users/juli/Documents/MoBi/Master/2_FS/Hermann_Praktikum/Analysis/spielerei/vipbaum.pdf",  width =4 , height = 4)
+#print(vipheat)
+#dev.off()
