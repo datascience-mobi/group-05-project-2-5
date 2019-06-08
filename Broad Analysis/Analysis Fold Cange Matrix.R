b5b18515f9a13534a76a0b397355c4e067a98894
Laura Plutowski <laura.plutki@gmail.com>
2019-06-08 15:43:01 +0200

    Update Analysis Fold Cange Matrix.R

--- Broad Analysis/Analysis Fold Cange Matrix.R
+++ Broad Analysis/Analysis Fold Cange Matrix.R
@@ -57,7 +57,7 @@ drug <- Metadata$drug
 palette(15)
 
 # creat boxplot
-barplot( height = FC_all_mean, names= FALSE, col = drug, border = NA)
+barplot( height = FC_all_mean, names= FALSE, col = drug, border = NA,main= "Fold change")
 
 # creat legend 
 levels <- as.factor(levels(drug))
@@ -69,6 +69,7 @@ legend("topright", inset = c(-0.3,0), legend= levels(drug), xpd = TRUE, pch=19, 
 
 ######################################### scatter plot ###########################################
 
+### drug
 plot(FC_all_mean, col= Metadata$drug, main="Fold change with drugs")
 # legend 
 drug <- Metadata$drug
@@ -81,8 +82,10 @@ plot(FC_all_mean, col= Metadata$tissue,main="Fold change with tissues")
 # legend 
 tissue <- Metadata$tissue
 levels <- as.factor(levels(tissue))
-legend("topright", inset = c(-0.3,0), legend= levels(tissue), xpd = TRUE, pch=19, col = levels, title = "drugs")
+legend("topright", inset = c(-0.3,0), legend= levels(tissue), xpd = TRUE, pch=19, col = levels, title = "tissues")
 # no correlation with tissue 
+
+
 
 
 ##################################################################################################
@@ -91,7 +94,7 @@ legend("topright", inset = c(-0.3,0), legend= levels(tissue), xpd = TRUE, pch=19
 
 ######################################### density plot ###########################################
 
-plot(density(FC_all_mean))
+plot(density(FC_all_mean), main= "density Fold change")
 
 # normally distributed 
 # most values between -0.75 and 0.5 
@@ -103,13 +106,24 @@ plot(density(FC_all_mean))
 
 #################################### find Top 10 values  #########################################
 
+### find all min and max 
 FC_all_min= (apply(FC_all,2,min))
 FC_all_max= (apply(FC_all,2,max))
 
+### sort min and max 
+
+# most down regulated gene
 largest10_FC_all_min <- (sort(FC_all_min, decreasing = F)[1:10])
-# most down regulated genes 
-largest10_FC_all_max <- (sort(FC_all_max, decreasing = T)[1:10])
+largest10_FC_all_min 
+# frequent drugs: vorinostat (3), bortezomib (3)
+# fequent cell line: OVCAR-4 (3)
+
 # most up regulated genes 
+largest10_FC_all_max <- (sort(FC_all_max, decreasing = T)[1:10])
+largest10_FC_all_max
+# frequent drugs: bortezomib (6)
+# fequent cell line: none
+
 
 
 
