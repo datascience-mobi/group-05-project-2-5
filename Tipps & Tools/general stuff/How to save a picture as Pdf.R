331ec382acd5d7d2154aa00c47b0c0465d0e289d
Mayadag <mayadag@hotmail.de>
2019-06-12 09:19:30 +0100

    .

--- Tipps & Tools/general stuff/How to save a picture as Pdf.R
+++
@@ -1,4 +0,0 @@
-############### How to save a picture ###############
-#pdf( "/Users/juli/Documents/MoBi/Master/2_FS/Hermann_Praktikum/Analysis/spielerei/vipbaum.pdf",  width =4 , height = 4)
-#print(vipheat)
-#dev.off()
