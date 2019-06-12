cb1fee0ba998692e1867d661aa26b51b04940ccb
Laura Plutowski <laura.plutki@gmail.com>
2019-06-12 10:42:28 +0200

    neu hochgeladen

---
+++ Pathways /KEGG test.R
@@ -0,0 +1,24 @@
+# KEGG over-representation test 
+###################################################################################################
+# instrall needed package 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("KEGG.db")
+
+library(KEGG.db)
+
+# see for other applications of this libary: 
+# https://bioconductor.org/packages/release/data/annotation/manuals/KEGG.db/man/KEGG.db.pdf
+
+###################################################################################################
+
+kk <- enrichKEGG(gene         = translated.genes,
+                 organism     = "human",
+                 pvalueCutoff = 0.05,
+                 readable= TRUE, 
+                 use_internal_data = TRUE)
+
+# here also problem with gene Id 
+
+###### Test
