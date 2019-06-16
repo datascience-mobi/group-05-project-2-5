9326ae91c9549f76bec0cfcdc8088455779d8789
FranziskaHeinkele <franziska.heinkele@arcor.de>
2019-06-16 14:13:23 +0200

    Create Visualization of Biomarkers Vulcano plot.R

---
+++ Specific Analysis/Visualization of Biomarkers Vulcano plot.R
@@ -0,0 +1,66 @@
+library(readr)
+library(rstudioapi)
+
+#load data:
+
+wd = dirname(rstudioapi::getSourceEditorContext()$path)
+
+Untreated = readRDS(paste0(wd,"/data/NCI_TPW_gep_untreated.rds"))
+Treated = readRDS(paste0(wd,"/data/NCI_TPW_gep_treated.rds"))
+Drug_annotation = read_tsv(paste0(wd,"/data/drug_annotation.tsv"))
+Cellline_annotation = read_tsv(paste0(wd,"/data/cellline_annotation.tsv"))
+Metadata = read_tsv(paste0(wd,"/data/NCI_TPW_metadata.tsv"))
+
+##First, load Vorinoistat data from file: "Create Vorinostat data"
+#This code goes on with FCnorm:
+
+#Perform a one-sample t.test with normalized FC data:
+T.testFCnorm <- apply(FCnorm, 1, t.test)
+
+#Save the results in a data frame:
+TResults <- lapply(T.testFCnorm, function(.tres) {      
+  
+  data.frame(t.value=.tres[1],dfs=.tres[2],conf.int1=.tres$conf.int[1],conf.int2=
+               .tres$conf.int[2],p.value=.tres[3])
+})
+
+Tfinalresults <- do.call(rbind, TResults)
+
+#For the vulcano plot, a package needs to be installed:
+
+if (!requireNamespace('BiocManager', quietly = TRUE))
+  install.packages('BiocManager')
+BiocManager::install('EnhancedVolcano')
+
+library(EnhancedVolcano)
+
+
+#Since we are interested in general biomarkers, we use the mean FC of the columns:
+log2FCnorm <- apply(FCnorm, 1, mean)
+
+#add the FCvalues as a new column to the data frame containing the results of the t.test:
+FCnormandpvalues <- cbind(Tfinalresults, log2FCnorm)
+
+#Create a Vulcanoplot:
+EnhancedVolcano(FCnormandpvalues,
+                lab = rownames(FCnormandpvalues),
+                x = 'log2FCnorm',
+                y = 'p.value',
+                xlim = c(-5, 8))
+
+#If we do not use the normalized FC values, the plot looks different:
+
+log2FC <- apply(FC, 1, mean)
+
+#add the FCvalues as a new column to the data frame containing the results of the t.test:
+FCandpvalues <- cbind(Tfinalresults, log2FC)
+
+#Create a Vulcanoplot:
+EnhancedVolcano(FCandpvalues,
+                lab = rownames(FCandpvalues),
+                x = 'log2FC',
+                y = 'p.value',
+                xlim = c(-5, 8))
+
+
+
