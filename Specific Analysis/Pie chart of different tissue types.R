272504221ac9a745e4fb454fed38cf752f56b21b
FranziskaHeinkele <franziska.heinkele@arcor.de>
2019-06-16 13:55:35 +0200

    Create Pie chart of different tissue types.R

---
+++ Specific Analysis/Pie chart of different tissue types.R
@@ -0,0 +1,26 @@
+library(readr)
+library(rstudioapi)
+
+wd = dirname(rstudioapi::getSourceEditorContext()$path)
+
+#Load data:
+
+Treated = readRDS(paste0(wd,"/data/NCI_TPW_gep_treated.rds"))
+Metadata = read_tsv(paste0(wd,"/data/NCI_TPW_metadata.tsv"))
+
+#Use tissue information from Metadata and add it to Treated-matrix, then select those columns which belong to Vorinostattreatment:
+
+Metadata <- as.matrix(Metadata)
+Metadatatissue <- Metadata[1:819,"tissue"]
+Treatedwithtissue <- rbind(Treated, Metadatatissue)
+
+    TreatedVorinostatcolumns <- grep(pattern = "vorinostat",colnames(Treatedwithtissue))
+    TreatedVorinostattissue <- Treatedwithtissue[,TreatedVorinostatcolumns]
+
+#Create a pie chart:
+    
+palette(rainbow(9))
+pie(table(TreatedVorinostattissue["Metadatatissue",]),
+    main="Tissue types of Vorinostat-treated samples", 
+    radius=1, 
+    cex=0.8)
