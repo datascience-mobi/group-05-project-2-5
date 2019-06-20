ffe2142df119e36a210ccf1c3a3ab8c0bf0deca3
Laura Plutowski <laura.plutki@gmail.com>
2019-06-20 17:46:24 +0200

    improved order

--- Specific Analysis/Sorting Biomarkers 2.R
+++
@@ -1,4 +0,0 @@
-#### For storting our Biomarker Matrix, Problem:without gene names 
-library(dplyr)
-Biomarker <- VorinostatwithpValues[VorinostatwithpValues$pValues <=  sortedpValues[100,],]
-Biomarker.sort2=arrange(Biomarker, Biomarker$pValues)
