48aad34e8d0d1e43816a138b568ac41186fd8d29
Florencia Zuniga Bräuning <50322377+flozuni@users.noreply.github.com>
2019-06-23 13:06:09 +0200

    Update Interactive biomarkers heatmap.R

--- Specific Analysis/Different Analysis/Interactive biomarkers heatmap.R
+++ Specific Analysis/Different Analysis/Interactive biomarkers heatmap.R
@@ -9,6 +9,25 @@ library(hexbin)
 library(devtools)
 library(heatmaply)
 
+#Load creat vorinostat and biomarkers over FC
+
+#Matrix for 100 biomarkers
+cor1.2_tab = FC [ which(row.names(FC) %in% rownames(biomarkers_FC_values100)), ]
+
+#Cleaning the names of the columns and rows
+#COLUMNS ONLY WITH CELL LINE + CHANGE NAME OF GENE HIST, CHANGE CELL LINES NAME'S TO TISSUES
+
+colnames(cor1.2_tab)
+col_names_cor1.2    = as.data.frame(strsplit(x=colnames(cor1.2_tab),split="_vorinostat"))
+colnames (cor1.2_tab) = as.data.frame (t(col_names_cor1.2[1,]))[,1]
+
+rownames(cor1.2_tab)
+row_names_cor1.2    = as.data.frame(strsplit(x=rownames(cor1.2_tab),split="///HIST"))
+rownames (cor1.2_tab) = as.data.frame (t(row_names_cor1.2[1,]))[,1]
+
+cor1.2_tab = as.matrix(cor1.2_tab)
+
+
 #Heatmap 
 heatmaply(cor1.2_tab, k_row = 2, k_col = 2)
 
