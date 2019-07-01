26e0f864271c99aa1cd51bfbc2e4cbb282138de1
Laura Plutowski <laura.plutki@gmail.com>
2019-07-01 19:25:46 +0200

    Update
    FC colored and pathway picture

--- Pathways/KEGG and Pathways.R
+++ Pathways/KEGG and Pathways.R
@@ -13,7 +13,11 @@
 library(KEGG.db)
 library(clusterProfiler)
 library(org.Hs.eg.db)
-gene2 <- row.names(biomarkers_FC)
+top100generalbiomarkers=as.data.frame(top100generalbiomarkers)
+colnames(top100generalbiomarkers)[1] <- "fold Change"
+top100generalbiomarkers=cbind(top100generalbiomarkers,generalbiomarkergenes)
+colnames(top100generalbiomarkers)[2] <- "genes"
+gene2 <- top100generalbiomarkers$genes
 gene.df <- bitr(gene2, fromType = "SYMBOL",
                 toType = c("ENSEMBL", "ENTREZID"),
                 OrgDb = org.Hs.eg.db)
@@ -40,17 +44,51 @@ kk3 <- enrichKEGG(gene=gene.df$ENTREZID,pvalueCutoff = 0.05,pAdjustMethod = "non
 head(summary(kk3))
 barplot(kk3,showCategory=100)
 dotplot(kk3, showCategory=12)
-cnetplot(kk3,categorySize="geneNum")
+cnetplot(kk3,categorySize="geneNum",foldChange=top100generalbiomarkers$`fold Change`)
 emapplot(kk3)
+
+
+
+####
+browseKEGG(kk, 'hsa05202')
+
+
+# colored FC 
+library(enrichplot)
+library(DOSE)
+top100generalbiomarkers=as.data.frame(top100generalbiomarkers)
+colnames(top100generalbiomarkers)[1] <- "fold Change"
+top100generalbiomarkers=cbind(top100generalbiomarkers,generalbiomarkergenes)
+colnames(top100generalbiomarkers)[2] <- "genes"
+
+FC <- top100generalbiomarkers$`fold Change`
+names(FC) <- gene.df$ENTREZID
+
+cnetplot(kk3,categorySize="pvalue",foldChange=FC)
+heatplot(kk3, foldChange=FC)
+
+
+## view pathway
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("pathview")
+library(pathview)
+pathview(gene.data = FC, 
+         pathway.id = "hsa05202", 
+         species = "hsa", 
+         limit = list(gene=5, cpd=1))
+
+
 
 ###################################################################################################
 # following enrichments based on this web page: 
 # https://bioconductor.org/packages/release/bioc/vignettes/ReactomePA/inst/doc/ReactomePA.html#pathway-enrichment-analysis
 ###################################################################################################
-if (!requireNamespace("BiocManager", quietly = TRUE))
+'if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-BiocManager::install("ReactomePA")
+BiocManager::install("ReactomePA")'
 
 library(ReactomePA)
 
@@ -62,5 +100,7 @@ barplot(x,showCategory=12)
 dotplot(x, showCategory=12)
 cnetplot(x,categorySize="geneNum")
 emapplot(x)
+
+
 
 
