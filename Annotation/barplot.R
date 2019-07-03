10b93015488d975800fe93a7f396d34d83f1c125
Laura Plutowski <laura.plutki@gmail.com>
2019-07-03 11:51:31 +0200

    Update barplot.R

--- Annotation/barplot.R
+++ Annotation/barplot.R
@@ -1,5 +1,5 @@
 
-# copy biomarker and Biomarker3 from annotation to data 
+# copy Biomarkers from annotation to data 
 # barplot with diffrent tissues 
 
 library(readr)
@@ -8,11 +8,13 @@ library(rstudioapi)
 wd = dirname(rstudioapi::getSourceEditorContext()$path)
 
 
-vorinostat_annotation=read.csv2(paste0(wd,"/../data/biomarker.csv"),header = TRUE, quote="\"")
+vorinostat_annotation=read.csv2(paste0(wd,"/../data/Biomarkers.csv"),header = TRUE, quote="\"")
 
 tissue=vorinostat_annotation$affected.Tissue..if.specific.
 
-col=palette(rainbow(6))
+
+
+col=palette(rainbow(9))
 table(tissue)
 barplot(table(tissue), ylab="counts", main="affected tissues by biomarker", col=col, las=3)
 
@@ -25,3 +27,12 @@ tissue=vorinostat_annotation$affected.Tissue..if.specific.
 col=palette(rainbow(7))
 table(tissue)
 barplot(table(tissue), ylab="counts", main="affected tissues by biomarker", col=col, las=3)
+
+#### barplot functions 
+general.function=vorinostat_annotation$general.Function
+
+col=palette(rainbow(20))
+table(general.function) 
+barplot(table(general.function), ylab="counts", main="general function biomarker", col=col, las=3)
+
+
